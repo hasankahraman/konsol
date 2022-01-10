22d6b323dcd38a1bf928fd38974a188aa762b052
hasankahraman <hasankahraman06092@gmail.com>
2022-01-10 22:14:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Lütfen isminizi giriniz: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Lütfen soy isminizi giriniz: ");
+string surName = Console.ReadLine();
+
+Console.WriteLine("Merhaba " + name + " " + surName );
+
